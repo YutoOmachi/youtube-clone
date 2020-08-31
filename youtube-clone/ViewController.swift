cc813b751cec1b1b11943f0f0117c21ed8558936
Yuto Omachi <youomachi@gmail.com>
2020-08-31 16:58:42 +0900

    initial xcode project creation

---
+++ youtube-clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-clone
+//
+//  Created by 小町悠登 on 31/8/20.
+//  Copyright © 2020 Yuto Omachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
