268e22b3ad72de901cd6e5aa9f6028ee2d372da8
Yuto Omachi <youomachi@gmail.com>
2020-09-02 10:02:53 +0900

    added template file for constants file

---
+++ youtube-clone/ConstantsTemplate.swift
@@ -0,0 +1,17 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-clone
+//
+//  Created by 小町悠登 on 2/9/20.
+//  Copyright © 2020 Yuto Omachi. All rights reserved.
+//
+
+import Foundation
+
+struct ConstantsTemplate {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    
+}
